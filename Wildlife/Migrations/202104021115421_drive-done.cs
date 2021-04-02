f2d20a9fd7beb0e4358e14953956324632cf18c3
itshawk <itshawk@gnode.org>
2021-04-02 01:28:45 -1000

    Complete button added, with database for it, hides buttons accordingly

---
+++ Wildlife/Migrations/202104021115421_drive-done.cs
@@ -0,0 +1,18 @@
+﻿namespace Wildlife.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class drivedone : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Drives", "DriveDone", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Drives", "DriveDone");
+        }
+    }
+}
